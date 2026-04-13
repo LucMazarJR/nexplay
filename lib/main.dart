1e58a75dce77895023bfaea813f3ea4a473250d2
LucianoMazar <lmazaraojr@gmail.com>
2026-04-13 19:08:08 -0300

    chore(core): prepare MVVM architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nexplay/app/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  runApp(const App());
 }
