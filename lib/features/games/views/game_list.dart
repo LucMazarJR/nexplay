709a1f01a315d8c354fbf6e8297906d6f9c180a8
LucianoMazar <lmazaraojr@gmail.com>
2026-06-30 18:02:27 -0300

    feat(games): add empty list page

--- lib/features/games/views/game_list.dart
+++ lib/features/games/views/game_list.dart
@@ -10,6 +10,64 @@ class GameList extends StatefulWidget {
 class _GameListState extends State<GameList> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final themeColors = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Meus jogos')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: themeColors.secondaryContainer,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Icon(
+                    Icons.sports_esports_outlined,
+                    color: themeColors.primary,
+                    size: 85,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Sua biblioteca está vazia',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Adicione o primeiro jogo da sua coleção para começar.',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: themeColors.primary,
+                  foregroundColor: themeColors.onPrimary,
+                ),
+                child: Row(
+                  spacing: 5,
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add, size: 20),
+                    Text('Adicionar jogo', style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
