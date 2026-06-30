f5f00c66cd58dca7863e5d6e92d4dfb0149a38d5
LucianoMazar <lmazaraojr@gmail.com>
2026-06-30 16:29:30 -0300

    chore(games): add game list initial component

---
+++ lib/features/games/views/game_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GameList extends StatefulWidget {
+  const GameList({super.key});
+
+  @override
+  State<GameList> createState() => _GameListState();
+}
+
+class _GameListState extends State<GameList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
