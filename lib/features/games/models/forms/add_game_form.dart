e6e5770f4f937b884f81efb73fc5dd9085904941
LucianoMazar <lmazaraojr@gmail.com>
2026-08-11 21:02:24 -0300

    refactor(games): rename Game class to GameForm for clarity

--- lib/features/games/models/forms/add_game_form.dart
+++ lib/features/games/models/forms/add_game_form.dart
@@ -2,12 +2,12 @@ import 'package:nexplay/features/games/models/class/genres.dart';
 import 'package:nexplay/features/games/models/class/tags.dart';
 import 'package:nexplay/features/games/models/enum/add_game_form.dart';
 
-class Game {
+class GameForm {
   String? name;
   GameStatus? status;
   Genre? genre;
   Set<Tag>? tags;
   String? imagePath;
 
-  Game();
+  GameForm();
 }
