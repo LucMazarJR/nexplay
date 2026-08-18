4f7bc926e52b1110b3e9ec6eeae7362a65f37da8
LucianoMazar <lmazaraojr@gmail.com>
2026-08-17 21:18:13 -0300

    feat(games): add toTitleCase function

--- lib/core/util.dart
+++ lib/core/util.dart
@@ -25,3 +25,12 @@ TextTheme createTextTheme(
   );
   return textTheme;
 }
+
+String toTitleCase(String text) {
+  if (text.isEmpty) return text;
+  
+  return text.split(' ').map((word) {
+    if (word.isEmpty) return word;
+    return word[0].toUpperCase() + word.substring(1).toLowerCase();
+  }).join(' ');
+}
