7abb532d4636c0d1a5e703827bd9a410e12ea0c2
LucianoMazar <lmazaraojr@gmail.com>
2026-08-17 21:02:12 -0300

    feat: add initial colors

--- lib/features/games/models/enum/game_status_style.dart
+++ lib/features/games/models/enum/game_status_style.dart
@@ -3,20 +3,20 @@ import 'package:nexplay/features/games/models/enum/game_status.dart';
 
 extension GameStatusStyle on GameStatus {
   Color get backgroundColor => switch (this) {
-    GameStatus.novo => Colors.blue,
-    GameStatus.nenhum => Colors.blue,
-    GameStatus.continuar => Colors.blue,
-    GameStatus.jogando => Colors.blue,
-    GameStatus.finalizado => Colors.blue,
-    GameStatus.abandonado => Colors.blue,
+    GameStatus.novo => Colors.blue[300] ?? Colors.blue,
+    GameStatus.nenhum => Colors.white,
+    GameStatus.continuar => Colors.orange[300] ?? Colors.orange,
+    GameStatus.jogando => Colors.purple[300] ?? Colors.purple,
+    GameStatus.finalizado => Colors.green[300] ?? Colors.green,
+    GameStatus.abandonado => Colors.grey[300] ?? Colors.grey,
   };
 
   Color get textColor => switch (this) {
-    GameStatus.novo => Colors.blue,
-    GameStatus.nenhum => Colors.blue,
-    GameStatus.continuar => Colors.blue,
-    GameStatus.jogando => Colors.blue,
-    GameStatus.finalizado => Colors.blue,
-    GameStatus.abandonado => Colors.blue,
+    GameStatus.novo => Colors.blue[900] ?? Colors.blue,
+    GameStatus.nenhum => Colors.white,
+    GameStatus.continuar => Colors.orange[900] ?? Colors.orange,
+    GameStatus.jogando => Colors.purple[900] ?? Colors.purple,
+    GameStatus.finalizado => Colors.green[900] ?? Colors.green,
+    GameStatus.abandonado => Colors.grey[900] ?? Colors.grey,
   };
 }
